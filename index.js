a503cda96e40997e2e5b065b138ca1ca88cb334b
carmenjoekalda <cjoekalda@gmail.com>
2024-05-16 14:28:29 +0300

    Created users table and added user data

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const sequelize = require('./utils/db')
+const sessions = require('express-session');
+
+const User = require('./models/user');
+User.sync();
 
 const app = express();
 
+app.use(sessions({
+    secret: 'thisismysecretkey',
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24}, // 24h
+    resave: false 
+}));
+
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const usersRoutes = require('./routes/users');
+
+app.use('/users', usersRoutes);
 
 app.listen(3012, () => {
     console.log('server is connected');
